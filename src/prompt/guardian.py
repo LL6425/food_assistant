45057a176944b8273624413d0fd596e3c9f1b664
ll6425 <ll.brt425@gmail.com>
2024-12-27 18:28:39 +0100

    refactor: using previously defined common instruments

--- src/prompt/guardian.py
+++ src/prompt/guardian.py
@@ -2,14 +2,10 @@ import json
 
 from src.conf.data_model.control import InputValidation
 
+
 class GuardianPrompt:
-
     def __init__(self):
-
-        self.system_description = f"Act as a guardian whose task is assessing whether or not the prompt is food related.\nThe JSON answer must use the schema: \n{json.dumps(InputValidation.model_json_schema(), indent=2)}"
+        self.system_description = f"Act as a guardian whose task is assessing whether or not the prompt is food related."
 
     def get_guardian_prompt(self, prompt: str) -> str:
-
-        return f"""The following request is provided: \n{prompt} \nIs it a food related request?"""
-
-    
+        return f"""The following request is provided: "{prompt}". Is it a food related request?"""
