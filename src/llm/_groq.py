bb2b9599a1c02cfbeccc82ab4f2f1f2d88a73ad1
ll6425 <ll.brt425@gmail.com>
2024-12-27 18:29:25 +0100

    refactor: moved functionalities to enhance generalized patterns

--- src/llm/_groq.py
+++ src/llm/_groq.py
@@ -1,29 +1,31 @@
+from typing import List, Dict, Type
+
 from groq import Groq
+from pydantic import BaseModel
 
 from src.prompt.guardian import GuardianPrompt
+from src.conf.data_model.llm import Guardian
 from src.conf.data_model.control import InputValidation
 
-
-class GroqGuardian:
+class GroqGuardian(Guardian):
     def __init__(
         self,
         groq_api_key: str,
-        prompt: GuardianPrompt,
+        guardian_prompt: GuardianPrompt = None,
         model: str = "llama3-70b-8192",
+        valid_structure: Type[BaseModel] = InputValidation
     ):
+        super().__init__(guardian_prompt=guardian_prompt, valid_structure=valid_structure)
+
         self.client = Groq(api_key=groq_api_key)
-        self.prompt = prompt
         self.model = model
 
-    def validate(self, prompt: str) -> bool:
+    def validate(self, history: List[Dict], prompt: str) -> bool:
+
+        history = self._complete_history(history=history, prompt=prompt)
+
         chat_completion = self.client.chat.completions.create(
-            messages=[
-                {"role": "system", "content": self.prompt.system_description},
-                {
-                    "role": "user",
-                    "content": self.prompt.get_guardian_prompt(prompt=prompt),
-                },
-            ],
+            messages=history,
             model=self.model,
             response_format={"type": "json_object"},
             temperature=0.0,
