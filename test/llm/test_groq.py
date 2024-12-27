e0a298e670f97d663377370432f7a24ace92f64d
ll6425 <ll.brt425@gmail.com>
2024-12-27 18:29:53 +0100

    test: added history

--- test/llm/test_groq.py
+++ test/llm/test_groq.py
@@ -5,16 +5,13 @@ import pytest
 from src.llm import GroqGuardian
 from src.prompt.guardian import GuardianPrompt
 
-def test_groq_guardian():
 
+def test_groq_guardian():
     guardian_prompt = GuardianPrompt()
 
-    guardian = GroqGuardian(groq_api_key=os.environ['GROQ_API_KEY'], prompt=guardian_prompt)
+    guardian = GroqGuardian(
+        groq_api_key=os.environ["GROQ_API_KEY"], guardian_prompt=guardian_prompt
+    )
 
-    assert guardian.validate('Provide lasagne recipe')
-    assert not guardian.validate('Code hello world in python')
-
-
-
-
-
+    assert guardian.validate([], "Provide lasagne recipe")
+    assert not guardian.validate([], "Code hello world in python")
