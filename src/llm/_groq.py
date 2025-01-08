f6a654c58e649dc9da6693bfcc01256da154bf36
ll6425 <ll.brt425@gmail.com>
2025-01-08 23:04:07 +0100

    feat: groq llm tools

--- src/llm/_groq.py
+++ src/llm/_groq.py
@@ -5,24 +5,43 @@ from pydantic import BaseModel
 
 from src.prompt.guardian import GuardianPrompt
 from src.conf.data_model.llm import Guardian
-from src.conf.data_model.control import InputValidation
+from src.conf.data_model.tools import RecipeSuggest, ChatBot
+from src.conf.data_model.output import Recipe, Text
+
 
 class GroqGuardian(Guardian):
     def __init__(
         self,
-        groq_api_key: str,
-        guardian_prompt: GuardianPrompt = None,
-        model: str = "llama3-70b-8192",
-        valid_structure: Type[BaseModel] = InputValidation
+        guardian_prompt: GuardianPrompt
     ):
-        super().__init__(guardian_prompt=guardian_prompt, valid_structure=valid_structure)
+        Guardian.__init__(self, guardian_prompt=guardian_prompt)
 
-        self.client = Groq(api_key=groq_api_key)
-        self.model = model
+    def validate(self, client: Groq, model: str, history: List[Dict], prompt: str) -> bool:
 
-    def validate(self, history: List[Dict], prompt: str) -> bool:
+        history = self.complete_history(history=history, prompt=prompt)
 
-        history = self._complete_history(history=history, prompt=prompt)
+        chat_completion = client.chat.completions.create(
+            messages=history,
+            model=model,
+            response_format={"type": "json_object"},
+            temperature=0.0,
+            stream=False
+        )
+
+        return self.output_structure.model_validate_json(
+            chat_completion.choices[0].message.content
+        ).is_valid
+    
+
+class GroqRecipeSuggest(RecipeSuggest):
+    def __init__(
+        self, client, model:str
+    ):
+        RecipeSuggest.__init__(self, client=client, model=model)
+
+    def __call__(self, inp: Text) -> Recipe:
+
+        history = self.require_struct_output(history=[], prompt=inp.text)
 
         chat_completion = self.client.chat.completions.create(
             messages=history,
@@ -32,6 +51,26 @@ class GroqGuardian(Guardian):
             stream=False
         )
 
-        return InputValidation.model_validate_json(
+        return self.json_to_model(
             chat_completion.choices[0].message.content
-        ).is_valid
+        )
+    
+class GroqChatBot(ChatBot):
+    def __init__(
+        self, client, model:str
+    ):
+        ChatBot.__init__(self, client=client, model=model)
+
+    def __call__(self, inp: Text) -> Text:
+
+        history = self.require_struct_output(history=[], prompt=inp.text)
+
+        chat_completion = self.client.chat.completions.create(
+            messages=history,
+            model=self.model,
+            response_format={"type": "json_object"},
+            temperature=0.0,
+            stream=False
+        )
+
+        return chat_completion.choices[0].message.content
